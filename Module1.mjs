8e81a1d9ead8ef9e42fcd6565b1f06e33b9c146a
Saurabh <saurabhchakraborty022@gmail.com>
2022-08-08 03:08:03 +0530

    Modules are modified <

--- Module1.mjs
+++ Module1.mjs
@@ -3,9 +3,9 @@ let b = 'dipankar';
 let c = 'sefali';
 let d = 'debasis';
 let e = 'unknown';
-export default d;
+export default c;
 export {a};
 export {b};
-export {c};
+export {d};
 export {e};
 
--- Module2.mjs
+++ Module2.mjs
@@ -1,6 +1,6 @@
-import xyz, {a,b,c} from './module1.mjs'
+import xyz, {a,b,d} from './module1.mjs'
 
 console.log(xyz);
 console.log(a);
 console.log(b);
-console.log(c);
+console.log(d);
