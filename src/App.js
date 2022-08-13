00453963f076c406ed0157c1f3b2562175218945
Saurabh <saurabhchakraborty022@gmail.com>
2022-08-13 21:21:02 +0530

    All Issues are Cleared

--- src/App.js
+++ src/App.js
@@ -1,17 +1,59 @@
 // import logo from './logo.svg';
 // import './App.css';
 import Navbar from './components/Navbar';
+import Alert from './components/Alert';
 import Forms from './components/Forms';
+import React, { useState } from 'react';
+import About from './components/About';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
+// import About from './components/About';
 
 function App() {
+  const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+  const showAlert =(message,type) =>{
+    setAlert({
+      msg: message,
+      typ: type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+  }
+  const toggleMode = () =>{
+    if(mode=== 'light'){
+    setMode('dark');
+    document.body.style.backgroundColor ='#10365b';
+    showAlert("Dark mode has been enabled", "success");
+  }
+  else{
+    setMode('light');
+    document.body.style.backgroundColor ='white';
+    showAlert("Light mode has been enabled", "success");
+  }
+  }
   return (
    <>
-   <Navbar  title = "Dictionary"/>
+   <Router>
+   <Navbar  title = "iCoder" mode = {mode} toggleMode = {toggleMode}/>
+   <Alert alert ={alert}/>
   <div className="container my-3"> 
-  <Forms headings = "Enter The Text Below"/>
+  <Routes>
+          <Route path="/about" element= {<About />}/>
+
+          <Route path="/" element = {<Forms headings = "Enter The Text Below" z = {showAlert} mode ={mode}/>}/>
+          <Route path="*" element= {<h1>Error 404! The Page is Not Found.</h1>}/>
+        </Routes>
   </div>
+   </Router>
    </>
   );
-}
 
+}
 export default App;
--- src/components/Forms.js
+++ src/components/Forms.js
@@ -5,29 +5,44 @@ export default function Forms(props) {
     // console.log("It is clicked.");
     let newText = text.toUpperCase();
     setText(newText);
+    props.z("converted to uppercase!", "success");
   };
   const handleLowClick = () => {
     // console.log("It is clicked.");
     let newText = text.toLowerCase();
     setText(newText);
+    props.z("converted to lowercase!", "success");
   };
   const handleClear = () => {
     // console.log("It is clicked.");
     let newText = '';
     setText(newText);
+    props.z("Its All Cleared!", "success");
   };
   const handleOnChange = (event) => {
     // console.log("It is changed.");
     setText(event.target.value);
   };
+  
+  const handleCopy = () =>{
+    let x = document.getElementById("enteredBox");
+    x.select();
+    navigator.clipboard.writeText(x.value);
+    props.z("Copied to clipboard!", "success");
+  }
+  const handleExtraSpaces = () =>{
+    let z = text.split(/[ ]+/);
+    setText(z.join(" "))
+    props.z("Extra Spaces are removed!", "success");
+  }
   const [text, setText] = useState('');
   return (
    <>
-   <div className="container my-2">
+   <div className="container my-2" style= {{color: props.mode === `dark`?`white`:`#10365b`}}>
       <h1>{props.headings}</h1>
       <div className="mb-3">
         <textarea
-          className="form-control"
+          className="form-control" style ={{ background : props.mode === `dark`?`grey`:`white`, color: props.mode === `dark`?`white`:`#10365b`}}
           value={text}
           onChange={handleOnChange}
           id="enteredBox"
@@ -39,15 +54,15 @@ export default function Forms(props) {
       </button>
       <button className="btn btn-primary mx-1" onClick={handleLowClick}>Submit for Lowercase</button>
       <button className="btn btn-danger mx-1" onClick={handleClear}>Clear Text</button>
+      <button className="btn btn-dark mx-1" onClick={handleCopy}>Copy Text</button>
+      <button className="btn btn-success mx-1" onClick={handleExtraSpaces}>Clear Extra Spaces</button>
     </div>
-    <div className ="container my-3">
+    <div className ="container my-3" style= {{color: props.mode === `dark`?`white`:`#10365b`}}>
 <h2>Text Summary</h2>
 <p>{text.split(" ").length} words and {text.length} characters.</p>
 <p>{0.008 *text.split(" ").length} minutes taken to read. </p>
 <h3>Preview</h3>
-<p>{text}</p>
-
-
+<p>{text.length>0?text:"Write Above Something for Preview."}</p>
     </div>
     </>
   );
